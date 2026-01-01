5f6a0773e693868a15db62e96b980b4f404da893
Daniel <dannica2k@users.noreply.github.com>
2025-12-31 16:00:14 -0800

    "v1.3.0_Fix_settings_screen_scrolling"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.energy20"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.2.9"
+        versionCode = 14
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
