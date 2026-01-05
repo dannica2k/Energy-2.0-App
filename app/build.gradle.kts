63a741a43be977131b920506f0b3f05956c59f9f
Daniel <dannica2k@users.noreply.github.com>
2026-01-04 16:20:10 -0800

    Bump version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.energy20"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.0.0"
+        versionCode = 16
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
